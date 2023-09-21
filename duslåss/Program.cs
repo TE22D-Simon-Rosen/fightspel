046c2a3d9218e53b16a4874f8ff5252ff39d5f5a
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2023-09-21 13:53:08 +0200

    hhhh

--- duslåss/Program.cs
+++ duslåss/Program.cs
@@ -1,33 +1,72 @@
 ﻿var random = new Random();
-int p1HP = 100;
-int p2HP = 100;
+Player player1 = new Player();
+Player player2 = new Player();
 
-string name;
-string name2 = "jeff";
-
+player2.name = "jeff";
 
 Console.WriteLine("Input your name");
-name = Console.ReadLine().Trim();
-if (string.IsNullOrEmpty(name)){
-    name = "Player 1";
+player1.name = Console.ReadLine().Trim();
+if (string.IsNullOrEmpty(player1.name))
+{
+    player1.name = "Player 1";
 }
 
 
 Console.WriteLine("Press Enter to contine");
 
-void startRound(){
+
+void attack(Player attacker, Player target)
+{
     int damage;
 
-    Console.WriteLine($"{name} HP - {p1HP}  |  {name2} HP - {p2HP}");
+    Console.WriteLine($"{attacker.name} Attacks!");
+
+    damage = random.Next(20, 50);
+    target.hp -= damage;
+
+    Console.WriteLine($"\n{attacker.name} did {damage} damage to {target.name}!");
     Console.ReadLine();
 
-    Console.WriteLine($"{name} Attacks!");
-    Console.ReadLine();
+    if (target.hp <= 0)
+    {
+        target.dead = true;
+    }
+}
 
 
-    damage = random.Next(10, 25);
-    p2HP -= damage;
+do
+{
+    if (player1.dead == false && player2.dead == false)
+    {
+        Console.WriteLine($"\n{player1.name} HP: {player1.hp}  |  {player2.name} HP: {player2.hp}");
+        Console.ReadLine();
+        attack(player1, player2);
+    }
 
-    Console.WriteLine($"");
-    
+    if (player1.dead == false && player2.dead == false)
+    {
+        Console.WriteLine($"\n{player1.name} HP: {player1.hp}  |  {player2.name} HP: {player2.hp}");
+        Console.ReadLine();
+        attack(player2, player1);
+    }
+} while (player1.hp >= 0 && player2.hp >= 0);
+
+
+if (player1.hp <= 0)
+{
+    Console.WriteLine($"You lost!");
+}
+else
+{
+    Console.WriteLine($"You won!");
+}
+
+Console.ReadLine();
+
+
+public class Player
+{
+    public string name;
+    public int hp = 100;
+    public bool dead = false;
 }
