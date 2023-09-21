07171fe6160e5904c834fb4f5651ac134274e55f
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2023-09-21 10:17:38 +0200

    dddd

---
+++ duslåss/Program.cs
@@ -0,0 +1,33 @@
+﻿var random = new Random();
+int p1HP = 100;
+int p2HP = 100;
+
+string name;
+string name2 = "jeff";
+
+
+Console.WriteLine("Input your name");
+name = Console.ReadLine().Trim();
+if (string.IsNullOrEmpty(name)){
+    name = "Player 1";
+}
+
+
+Console.WriteLine("Press Enter to contine");
+
+void startRound(){
+    int damage;
+
+    Console.WriteLine($"{name} HP - {p1HP}  |  {name2} HP - {p2HP}");
+    Console.ReadLine();
+
+    Console.WriteLine($"{name} Attacks!");
+    Console.ReadLine();
+
+
+    damage = random.Next(10, 25);
+    p2HP -= damage;
+
+    Console.WriteLine($"");
+    
+}
