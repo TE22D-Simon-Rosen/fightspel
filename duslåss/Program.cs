ba3a49a3626395d8c4405294d1bd506e87fe7d9c
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2023-09-22 22:31:48 +0200

    h

--- duslåss/Program.cs
+++ duslåss/Program.cs
@@ -3,6 +3,19 @@ Player player1 = new Player();
 Player player2 = new Player();
 
 player2.name = "jeff";
+player2.weapon = 0;
+
+
+//Weapons with their minimum damage, maximum damage and chance to hit in %
+object[][] weapons = {
+        new object[] {"Fists", 10, 30, 100},
+        new object[] {"Sword", 30, 40, 70},
+        new object[] {"Gun", 60, 80, 30},
+        new object[] {"osthyvel", 30, 60, 50},
+        new object[] {"kaktus", 40, 60, 70},
+        new object[] {"RPG", 101, 1000, 100}
+    };
+
 
 Console.WriteLine("Input your name");
 player1.name = Console.ReadLine().Trim();
@@ -12,30 +25,77 @@ if (string.IsNullOrEmpty(player1.name))
 }
 
 
-Console.WriteLine("Press Enter to contine");
+void selectWeapon()
+{
+    Console.WriteLine("Select a weapon by typing the corresponding number:");
+
+    //Prints the name of every weapon in order as well as its damage range and hit chance
+    for (int h = 0; h <= weapons.Count() - 1; h++)
+    {
+        Console.WriteLine($"{h + 1}: {weapons[h][0]} - Damage: {weapons[h][1]}-{weapons[h][2]}, {weapons[h][3]}% Hit chance");
+    }
+
+    string tempWeapon;
+
+    tempWeapon = Console.ReadLine().Trim();
+
+    //Forces player to type only one number
+    if (tempWeapon.Length <= 1 && int.TryParse(tempWeapon, out int i))
+    {
+        player1.weapon = Convert.ToInt32(tempWeapon);
+        if (player1.weapon <= weapons.Count() && player1.weapon >= 1)
+        {
+            player1.weapon -= 1;
+            Console.WriteLine($"Weapon Selected: {weapons[player1.weapon][0]}");
+        }
+        else
+        {
+            Console.WriteLine("Type only one number");
+            selectWeapon();
+        }
+    }
+    else
+    {
+        Console.WriteLine("Type only one number");
+        selectWeapon();
+    }
+}
+
 
 
 void attack(Player attacker, Player target)
 {
     int damage;
+    int hitChance = random.Next(0, 100);
 
     Console.WriteLine($"{attacker.name} Attacks!");
 
-    damage = random.Next(20, 50);
-    target.hp -= damage;
+    //If the randomly generated hit chance is higher than the hit% of the weapon then print "missed"
+    if (hitChance > Convert.ToInt32(weapons[attacker.weapon][3])){
+        Console.WriteLine($"{attacker.name} missed!");
+    }
+    else{
+        //Selects a random amount of damage between the weapons minimum and maximum damage
+        damage = random.Next(Convert.ToInt32(weapons[attacker.weapon][1]), Convert.ToInt32(weapons[attacker.weapon][2]));
+        target.hp -= damage;
 
-    Console.WriteLine($"\n{attacker.name} did {damage} damage to {target.name}!");
-    Console.ReadLine();
+        Console.WriteLine($"\n{attacker.name} hit their attack! {damage} damage");
+        Console.ReadLine();
+    }
 
-    if (target.hp <= 0)
-    {
+    if (target.hp < 1){
         target.dead = true;
     }
 }
 
 
+//Promts player to select a weapon
+selectWeapon();
+
+//Starts game
 do
-{
+{   
+    //Player 1 attack
     if (player1.dead == false && player2.dead == false)
     {
         Console.WriteLine($"\n{player1.name} HP: {player1.hp}  |  {player2.name} HP: {player2.hp}");
@@ -43,6 +103,7 @@ do
         attack(player1, player2);
     }
 
+    //Player 2 attack
     if (player1.dead == false && player2.dead == false)
     {
         Console.WriteLine($"\n{player1.name} HP: {player1.hp}  |  {player2.name} HP: {player2.hp}");
@@ -69,4 +130,5 @@ public class Player
     public string name;
     public int hp = 100;
     public bool dead = false;
+    public int weapon;
 }
